434604812d4a81b4e91abbbecc86235b311fe8bf
Pedro Ferreira <pfsn.contato@gmail.com>
2023-11-08 06:54:59 -0300

    Acrescentado calculo digito 2 da validação do cpf

--- Exercicios/validar_cpf.py
+++ Exercicios/validar_cpf.py
@@ -19,8 +19,8 @@ contrário disso:
     resultado é o valor da conta
 O primeiro dígito do cpf é 7
 '''
-cpf = '04852991359'
-nove_digitos = cpf[:9] # fatiamento que vai do 0 ao 9
+cpf_enviado_usuario = '04852991359'
+nove_digitos = cpf_enviado_usuario[:9] # fatiamento que vai do 0 ao 9
 
 contador_regressivo_1 = 10
 
@@ -30,4 +30,20 @@ for digito_1 in nove_digitos:
     contador_regressivo_1 -= 1
 digito_1 = (resultado_digito_1 * 10) % 11
 digito_1 = digito_1 if digito_1 <= 9 else 0
-print(digito_1)
+
+dez_digitos = nove_digitos + str(digito_1)
+contador_regressivo_2 = 11
+
+resultado_digito_2 = 0
+for digito in dez_digitos:
+    resultado_digito_2 += int(digito) * contador_regressivo_2
+    contador_regressivo_2 -= 1
+digito_2 = (resultado_digito_2 * 10) % 11
+digito_2 = digito_2 if digito_2 <= 9 else 0
+
+cpf_gerado_pelo_calculo = f'{nove_digitos}{digito_1}{digito_2}'
+
+if cpf_enviado_usuario == cpf_gerado_pelo_calculo:
+    print(f'{cpf_enviado_usuario} é válido.')
+else:
+    print('cpf inválido')
