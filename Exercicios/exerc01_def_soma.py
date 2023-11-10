23a04d1e2fdaf76baf67229160c869193bdd9414
Pedro Ferreira <pfsn.contato@gmail.com>
2023-11-09 21:00:43 -0300

    Função def para maior número

---
+++ Exercicios/exerc01_def_soma.py
@@ -0,0 +1,9 @@
+'''
+Soma de Dois Números:
+Escreva uma função chamada soma que recebe dois números como argumentos e retorna a soma deles.
+'''
+def soma(x, y):
+    resultado = x + y
+    print(f'A soma de {x} + {y} = {resultado}')
+
+soma(2, 2)
