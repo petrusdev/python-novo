0d077f233fcff7ac741ad73f9581379dd69049f0
Pedro Ferreira <pfsn.contato@gmail.com>
2023-11-18 19:41:01 -0300

    Coloquei 3 aspas para meu print com mais de uma linha e melhorei simplicando minhas funções de bin, oct, hex para dentro da print de respostas, pois retirei as variáveis que convertiam.

--- Guanabara_python/exerc34_mundo2.py
+++ Guanabara_python/exerc34_mundo2.py
@@ -6,10 +6,23 @@ conversão
 - 3 para hexadecimal
 '''
 num_int = int(input('Digite um número inteiro: ')) 
-print('Escolha a opção:\n1 - Binário\n2 - Octal\n3 - Hexadecimal')
+print('''Escolha uma das opções:
+1 - BINÁRIO
+2 - OCTAL
+3 - HEXADECIMAL''')
 escolha = int(input('Escolha a opção: '))
 
-numero_binario = bin(num_int)[2:]
+if escolha == 1:
+    print(f'A representação binária de {num_int} é: {bin(num_int)[2:]}')
+elif escolha == 2:
+    print(f'A representação octal de {num_int} é: {oct(num_int)[2:]}')
+elif escolha == 3:
+    print(f'A representação hexadecimal de {num_int} é: {hex(num_int)[2:]}')
+else:
+    print('Opção inválida. Tente de novo.')
+          
+
+'''numero_binario = bin(num_int)[2:]
 numero_octal = oct(num_int)[2:]
 numero_hex = hex(num_int)[2:]
 
@@ -18,4 +31,4 @@ if escolha == 1:
 elif escolha == 2:
     print(f'A representação octal de {num_int} é: {numero_octal}')
 else:
-    print(f'A representação hexadecimal de {num_int} é: {numero_hex}')
+    print(f'A representação hexadecimal de {num_int} é: {numero_hex}')'''
