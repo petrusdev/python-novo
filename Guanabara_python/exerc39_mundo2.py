708c7a5c8a48e1d6478fe424671333f80df5081f
Pedro Ferreira <pfsn.contato@gmail.com>
2023-11-19 10:00:19 -0300

    Otimizei minha condição igual r1, r2 e r3

--- Guanabara_python/exerc39_mundo2.py
+++ Guanabara_python/exerc39_mundo2.py
@@ -12,7 +12,7 @@ r3 = float(input('Terceiro segmento: '))
 
 if r1 < r2 + r3 and r2 < r1 + r3 and r3 < r1 + r2:
     print('Os segmentos acima PODEM FORMA um \033[33mtriângulo\033[m.')
-    if r1 == r2 and r1 == r3 and r2 == r3:
+    if r1 == r2 == r3:
         print('O triângulo formado é: EQUILÁTERO.')
     elif r1 == r2 or r1 == r3 or r2 == r3:
         print('O triângulo formado: ISÓSCELES.')
