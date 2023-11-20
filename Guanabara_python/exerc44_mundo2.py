880c09d8d01fb99035008b60770702f44ac72155
Pedro Ferreira <pfsn.contato@gmail.com>
2023-11-20 06:56:19 -0300

    Contagem regressiva de ano novo com intervalo de 1 segundo em cada segundo

---
+++ Guanabara_python/exerc44_mundo2.py
@@ -0,0 +1,11 @@
+'''
+Faça um programa que mostre na tela uma contagem regressiva para
+o esrouro de fogos de artifício, indo de 10 até 0, com uma pausa de 1 segundo entre eles
+'''
+from time import sleep
+
+for c in range(10, 0, -1):
+    sleep(1)
+    print(c)
+sleep(1)
+print('FELIZZZZZ ANO NOVO!!!')
